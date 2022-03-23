e56c5b3a662a7b6be7f7daa1e26d66c914603d07
strcurseten <fernandez.kirstenpearl@auf.edu.ph>
2022-03-23 18:10:50 +0800

    Finalized the design

--- shopping-cart.php
+++ shopping-cart.php
@@ -31,7 +31,7 @@ $shoppingCartItems = $shoppingCart->getAllItems();
 
                     <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
                         <li>
-                        <a href="products-list.php" class="nav-link text-secondary">
+                        <a href="products-list.php" class="nav-link text-white">
                             <div class="fs-4 mb-3">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house-heart" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.707L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.646a.5.5 0 0 0 .708-.707L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207l-5-5-5 5V13.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7.207Zm-5-.225C9.664 5.309 13.825 8.236 8 12 2.175 8.236 6.336 5.309 8 6.982Z"/>
@@ -40,7 +40,7 @@ $shoppingCartItems = $shoppingCart->getAllItems();
                         </a>
                         </li>
                         <li>
-                        <a href="shopping-cart.php" class="nav-link text-white">
+                        <a href="shopping-cart.php" class="nav-link text-secondary">
                         <div class="fs-4 mb-3">
 
                             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
@@ -59,6 +59,10 @@ $shoppingCartItems = $shoppingCart->getAllItems();
     #bag{
         width: 1200px;
         padding: 50px 50px;
+    }
+    body {
+        overflow-x: hidden;
+        background-image: linear-gradient(120deg, #061a3f 50%, #ff7ea5 50%);
     }
 </style>
 
