b6c16a2bd66bd5a1ec1ae612fa362e45a0c98d89
strcurseten <fernandez.kirstenpearl@auf.edu.ph>
2022-03-23 16:48:57 +0800

    Designed the interface

--- shopping-cart.php
+++ shopping-cart.php
@@ -17,13 +17,63 @@ $shoppingCartItems = $shoppingCart->getAllItems();
 <head>
     <title>My Cart</title>
 </head>
-<body>
 
-<h1>Welcome <?php echo $customer->getName() ?>!</h1>
-<h2>Shopping Cart</h2>
-<h4>
-    <a href="products-list.php">Shop More Products</a>
-</h4>
+<link href="/docs/4.4/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        
+        <header>
+            <div class="px-3 py-2 bg-dark text-white">
+                <div class="container">
+                    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+                        <img src="kkum.png" width="130" height="50">
+                    </a>
+
+                    <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+                        <li>
+                        <a href="products-list.php" class="nav-link text-secondary">
+                            <div class="fs-4 mb-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house-heart" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.707L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.646a.5.5 0 0 0 .708-.707L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207l-5-5-5 5V13.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7.207Zm-5-.225C9.664 5.309 13.825 8.236 8 12 2.175 8.236 6.336 5.309 8 6.982Z"/>
+                                </svg>
+                            </div>
+                        </a>
+                        </li>
+                        <li>
+                        <a href="shopping-cart.php" class="nav-link text-white">
+                        <div class="fs-4 mb-3">
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-bag-heart" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5Zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0ZM14 14V5H2v9a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1ZM8 7.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
+                            </svg>
+                        </div>
+                        </a>
+                    </ul>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </header>
+
+<style>
+    #bag{
+        width: 1200px;
+        padding: 50px 50px;
+    }
+</style>
+
+
+<body>
+    <div class="container-fluid" id="bag">
+        <div class="card">
+        <h5 class="card-header">Shopping Bag</h5>
+            <div class="card-body">
+                <h5 class="card-title"><?php echo $customer->getName() ?>, your bag is currently empty.</h5>
+                <p class="card-text">Check out our products list and see what you'd like!</p>
+                <a href="products-list.php" class="btn btn-primary">Shop More Products</a>
+            </div>
+        </div>
+    </div>
 
 <?php if (count($shoppingCartItems) > 0): ?>
 
@@ -57,6 +107,6 @@ $shoppingCartItems = $shoppingCart->getAllItems();
     </table>
 
     <?php endif; ?>
-
+    <script src="/docs/5.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
